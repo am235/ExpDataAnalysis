6f5df6c9bd5a823bc9d2cfef39fdbc34d0424773
am235 <arindam235@gmail.com>
2016-12-18 00:05:55 +0530

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -5,11 +5,11 @@
 ## Plot 4 : All graphs together
 setwd("C:\\Users\\Arindam\\Desktop\\RWk1\\exdata")
 
-energy_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
-energy_data_subset <- subset(energy_data, Date == "2/2/2007" | Date == "1/2/2007")
+energy_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",stringsAsFactors=FALSE, dec=".")
+energy_data_subset <- energy_data[energy_data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 ##Converting the power to Kilowatts
-energy_data_subset$Global_active_power <- as.numeric(energy_data_subset$Global_active_power)/1000
+energy_data_subset$Global_active_power <- as.numeric(energy_data_subset$Global_active_power)
 
 #Converting to Date
 energy_data_subset$DateNew <- as.Date(as.character(energy_data_subset$Date),"%d/%m/%Y")
@@ -25,7 +25,7 @@ energy_data_subset$Sub_metering_3 <- as.numeric(energy_data_subset$Sub_metering_
 
 ##Plot 4 - all 4 in the sub graph
 par(mfrow=c(2,2)) # all plots on one page 
-plot(energy_data_subset$timeDt, energy_data_subset$Global_active_power,type="l",ylab="Global Active Power (Kilowatts)",xlab="")
+plot(energy_data_subset$timeDt, energy_data_subset$Global_active_power,type="l",ylab="Global Active Power (Kilowatts)",xlab="",cex=0.2)
 
 plot(energy_data_subset$timeDt, energy_data_subset$Voltage,type="l",ylab="Voltage",xlab="datetime")
 
@@ -33,9 +33,10 @@ plot(energy_data_subset$timeDt, energy_data_subset$Voltage,type="l",ylab="Voltag
 plot(energy_data_subset$timeDt, energy_data_subset$Sub_metering_1, type="l", ylab="Energy Sub Metering",xlab="")
 lines(energy_data_subset$timeDt, energy_data_subset$Sub_metering_2, col="red",xaxt="n",yaxt="n")
 lines(energy_data_subset$timeDt, energy_data_subset$Sub_metering_3, col="blue",xaxt="n",yaxt="n")
-legend("topright",legend=c("Sub_Metering_1","Sub_Metering_2","Sub_Metering_3"),box.col = "white", xjust=1, col=c("black","red","blue"),box.lty=0, text.font=0, lty=1:3,cex=0.5)
+legend("topright",legend=c("Sub_Metering_1","Sub_Metering_2","Sub_Metering_3"),xjust=1, col=c("black","red","blue"),box.lty=0, text.font=0, lty=1:3,cex=0.5)
 
 
 plot(energy_data_subset$timeDt, energy_data_subset$Global_reactive_power,type="l",ylab="Global Inactive Power",xlab="datetime")
-dev.copy(png, file = "plot4.png")
+
+dev.copy(png, file = "plot4.png", width=480, height=480)
 dev.off()
