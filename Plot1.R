320dc8514bb5534f3aa0f09ff0633e2581d94406
am235 <arindam235@gmail.com>
2016-12-18 00:04:08 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -5,15 +5,15 @@
 ## Plot 1 : Creating a Histogram
 setwd("C:\\Users\\Arindam\\Desktop\\RWk1\\exdata")
 
-energy_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
-energy_data_subset <- subset(energy_data, Date == "2/2/2007" | Date == "1/2/2007")
+energy_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",stringsAsFactors=FALSE, dec=".")
+energy_data_subset <- energy_data[energy_data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 ##Converting the power to Kilowatts
-energy_data_subset$Global_active_power <- as.numeric(energy_data_subset$Global_active_power)/1000
+energy_data_subset$Global_active_power <- as.numeric(energy_data_subset$Global_active_power)
 
 
 #Plot 1 - Histogram
 par(mfrow=c(1,1))
-hist(energy_data_subset$Global_active_power, ylim = c(0,2000), xlim = c(0,3), cex.main = 0.8, col = "red", breaks = 12, labels = TRUE, main="Global Active Power", xlab = "Global Active Power (Kilowatts)") 
-dev.copy(png, file = "plot1.png")
+hist(energy_data_subset$Global_active_power, freq = TRUE, cex.main = 0.8, col = "red", main="Global Active Power", xlab = "Global Active Power (Kilowatts)") 
+dev.copy(png, file = "plot1.png", width=480, height=480)
 dev.off()
